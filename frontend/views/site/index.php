24a0e0f6cf485403c4774ee7780c5cb72779d748
Sempurna 1 <profesorrob01@gmail.com>
2022-06-16 09:17:57 +0700

    Fix Code

--- frontend/views/site/index.php
+++ frontend/views/site/index.php
@@ -1,9 +1,13 @@
 <?php
 
+use yii\helpers\Html;
 use yii\grid\GridView;
 use frontend\models\Item;
 use yii\widgets\ListView;
+use yii\widgets\ActiveForm;
+use yii\bootstrap4\LinkPager;
 use yii\data\ActiveDataProvider;
+use frontend\models\ItemCategory;
 
 /** @var yii\web\View $this */
 
@@ -19,14 +23,37 @@ $this->title = 'Warung Ku';
         <!-- <p><a class="btn btn-lg btn-success" href="http://www.yiiframework.com">Get started with Yii</a></p> -->
     </div>
 
-    <?php
-    $dataProvider = new ActiveDataProvider([
-        'query' => Item::find(),
-        'pagination' => [
-            'pageSize' => 10,
-        ],
-    ]);
+    <?php $form = ActiveForm::begin(['action' => ['site/index'], 'options' => ['method' => 'post']]); ?>
+    <center>
+        <select name="category">
+            <option value="0">Select Category</option>
+            <?php foreach (ItemCategory::find()->all() as $c) : ?>
+                <option value="<?= $c->id; ?>"><?= $c->name; ?></option>
+            <?php endforeach; ?>
+        </select>
+
+        <?= Html::submitButton('Filter', ['class' => 'btn btn-success']) ?>
+    </center>
+
+    <?php ActiveForm::end(); ?>
+
+    <?php foreach ($models as $model) : ?>
+        <?= $this->render('_list_item', ['model' => $model]); ?>
+        <br />
+    <?php endforeach; ?>
+    <?=
+    LinkPager::widget([
+        'pagination' => $pagination
+    ])
     ?>
+    <!-- <?php
+            $dataProvider = new ActiveDataProvider([
+                'query' => Item::find(),
+                'pagination' => [
+                    'pageSize' => 10,
+                ],
+            ]);
+            ?>
     <?=
     ListView::widget([
         'dataProvider' => $dataProvider,
@@ -50,7 +77,8 @@ $this->title = 'Warung Ku';
     //         ],
     //     ],
     // ]);
-    ?>
+    // 
+    ?> -->
 
     <!-- <div class="body-content">
 
